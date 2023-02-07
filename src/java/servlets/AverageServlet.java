fce41f161a988c2dfd9c7ee0eff8e0aca40bbd45
m-amirfakhrian <majid.amirfakhrian@edu.sait.ca>
2023-02-06 21:45:14 -0700

    Adding an ArrayList

--- src/java/servlets/AverageServlet.java
+++ src/java/servlets/AverageServlet.java
@@ -3,10 +3,12 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -19,6 +21,13 @@ public class AverageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        HttpSession session = request.getSession();
+        
+        ArrayList<Integer> numbers = (ArrayList<Integer>) session.getAttribute("numbers");
+        if (numbers == null)
+            numbers = new ArrayList<Integer>();
+        
      getServletContext().getRequestDispatcher("/WEB-INF/average.jsp").forward(request, response);
     }
 
